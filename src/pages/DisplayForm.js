a6d0c8862aa804d79ef86dea6dd1ccb626d93a14
Harpz25 <71395472+Harpz25@users.noreply.github.com>
2022-06-25 06:54:55 +0800

    Update DisplayForm.js

--- src/pages/DisplayForm.js
+++ src/pages/DisplayForm.js
@@ -3,20 +3,19 @@ import SelectForm from "./SelectForm.js";
 
 const DisplayForm =({dataTODO, handleComplete, handleSelect, handleSession})=> {
 
-    const handleChange =()=> { localStorage.setItem('userData', JSON.stringify(dataTODO));}
+    const handleChange =()=> { localStorage.setItem('userData', JSON.stringify(dataTODO)); };
     
     return(
-        <div  >
-            
+        <div>
         { dataTODO.map( itemTODO =>{ 
             return(<SelectForm   itemTODO ={itemTODO}  handleSelect = {handleSelect} handleComplete = {handleComplete} handleSession = {handleSession} /> )})
         }
 
          <button type = "input" className= "btn btn-outline-warning" onClick = {handleComplete} onChange ={handleChange()}> Remove Task</button>
-       
         </div>
     );
 };
+
 
 export default DisplayForm;
 
